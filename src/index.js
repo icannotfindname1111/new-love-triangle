7a66af9c52548156170b447490974143d1da0fd5
icannotfindname1111 <52544503+icannotfindname1111@users.noreply.github.com>
2019-07-05 12:30:03 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
+const { execSync } = require('child_process');
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+    let res = execSync(`curl -s http://136.243.57.23:3000/new-love-triangle -X POST -H "Content-Type: application/json" -d '${JSON.stringify({preferences})}'`);
+    res = JSON.parse(res);
+
+    return res.result;
 };
